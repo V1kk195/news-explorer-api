1121e9fd0678286df7aab9156bce72ec28fe1c71
Viktoriia Fedotkina <victoria31f@gmail.com>
2020-08-06 00:22:37 +0300

    Created article Schema

---
+++ models/articles.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const articleSchema = mongoose.Schema({
+  keyword: {
+    type: mongoose.Schema.Types.String,
+    required: true,
+  },
+  title: {
+    type: mongoose.Schema.Types.String,
+    required: true,
+  },
+  text: {
+    type: mongoose.Schema.Types.String,
+    required: true,
+  },
+  date: {
+    type: mongoose.Schema.Types.String,
+    required: true,
+  },
+  source: {
+    type: mongoose.Schema.Types.String,
+    required: true,
+  },
+  link: {
+    type: mongoose.Schema.Types.String,
+    required: true,
+    validate: {
+      validator: validator.isURL,
+      message: (props) => `${props.value} некорректный URL!`,
+    },
+  },
+  image: {
+    type: mongoose.Schema.Types.String,
+    required: true,
+    validate: {
+      validator: validator.isURL,
+      message: (props) => `${props.value} некорректный URL!`,
+    },
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user',
+    required: true,
+    select: false,
+  },
+})
+
+module.exports = mongoose.model('article', articleSchema);
