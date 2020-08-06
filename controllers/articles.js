6dca3dc7ddfc814c09643573ab92b7072c600a3e
Viktoriia Fedotkina <victoria31f@gmail.com>
2020-08-06 23:38:03 +0300

    Created articles controllers

---
+++ controllers/articles.js
@@ -0,0 +1,34 @@
+const Articles = require('../models/articles');
+
+module.exports.getArticles = (req, res, next) => {
+  Articles.find({})
+    .then((articles) => {
+      res.send({ data: articles });
+    })
+    .catch(next);
+};
+
+module.exports.createArticle = (req, res, next) => {
+  const {
+    keyword, title, text, date, source, link, image,
+  } = req.body;
+  Articles.create({
+    keyword, title, text, date, source, link, image,
+  })
+    .then((article) => {
+      res.send({ data: article });
+    })
+    .catch(next);
+};
+
+module.exports.deleteArticle = (req, res, next) => {
+  Articles.findById(req.params.articleId)
+    .orFail()
+    .then((article) => {
+      if (!article.owner.equals(req.user._id)) {
+        return Promise.reject(new ForbiddenError('Вы пытаетесь удалить чужую статью'));
+      }
+      return Articles.deleteOne(article);
+    })
+    .catch(next);
+};
