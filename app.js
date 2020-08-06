8b5d17bef2390bac549a4577c4172d3d6ebca22c
Viktoriia Fedotkina <victoria31f@gmail.com>
2020-08-07 00:09:26 +0300

    Added central error handler

--- app.js
+++ app.js
@@ -15,4 +15,16 @@ mongoose.connect('mongodb://localhost:27017/news-explorer', {
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// eslint-disable-next-line no-unused-vars
+app.use((err, req, res, next) => {
+  const { statusCode = 500, message } = err;
+  res
+    .status(statusCode)
+    .send({
+      message: statusCode === 500
+        ? 'На сервере произошла ошибка'
+        : message,
+    });
+});
+
 app.listen(PORT);
