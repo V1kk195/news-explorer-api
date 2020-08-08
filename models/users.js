1663ba24c0b6748f89ae24e122ac8bbfd3c9ff57
Viktoriia Fedotkina <victoria31f@gmail.com>
2020-08-08 13:37:46 +0300

    Added findUserByCredentials method in Users model

--- models/users.js
+++ models/users.js
@@ -1,5 +1,8 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
+
+const UnauthorisedError = require('../errors/unauthorized-error');
 
 const userSchema = mongoose.Schema({
   email: {
@@ -23,5 +26,22 @@ const userSchema = mongoose.Schema({
     maxlength: 30,
   },
 });
+
+// eslint-disable-next-line func-names
+userSchema.statics.findUserByCredentials = function (email, password) {
+  return this.findOne({ email })
+    .then((user) => {
+      if (!user) {
+        throw new UnauthorisedError('Неправильные почта или пароль');
+      }
+      return bcrypt.compare(password, user.password)
+        .then((matched) => {
+          if (!matched) {
+            throw new UnauthorisedError('Неправильные почта или пароль');
+          }
+          return user;
+        });
+    });
+};
 
 module.exports = mongoose.model('user', userSchema);
