6a2278321dbea38c653fdb7c38f8f57d1157277f
Viktoriia Fedotkina <victoria31f@gmail.com>
2020-09-06 16:22:12 +0300

    Added CORS

--- app.js
+++ app.js
@@ -5,11 +5,28 @@ const cookieParser = require('cookie-parser');
 require('dotenv').config();
 const { errors } = require('celebrate');
 const helmet = require('helmet');
+const cors = require('cors');
 
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const routes = require('./routes/index');
 const { limiter, mongoAddress } = require('./config');
 const errorHandler = require('./middlewares/error-handler');
+
+const whitelist = [
+  'http://localhost:8080/', 'https://api.explorerofnews.ga', 'http://api.explorerofnews.ga', 'https://explorerofnews.ga',
+  'http://explorerofnews.ga',
+];
+
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true,
+};
 
 const { PORT = 3000 } = process.env;
 
@@ -26,6 +43,7 @@ app.use(limiter);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(cors(corsOptions));
 app.use(requestLogger);
 app.use(routes);
 app.use(errorLogger);
