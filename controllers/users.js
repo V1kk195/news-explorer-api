a931e18da4b4ba517083972a371af152bada61b8
Viktoriia Fedotkina <victoria31f@gmail.com>
2020-10-28 21:02:46 +0300

    added user id in get user info

--- controllers/users.js
+++ controllers/users.js
@@ -24,6 +24,7 @@ module.exports.getUser = (req, res, next) => {
           data: {
             email: user.email,
             name: user.name,
+            id: user._id,
           },
         });
     })
